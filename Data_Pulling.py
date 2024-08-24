7d7c4a8fb9788498660786aa61c1c6f27e79e463
Derik Thiessen <derikt03@live.com>
2024-08-24 15:58:20 -0400

    Changed files to protect the API key

--- Data_Pulling.py
+++ Data_Pulling.py
@@ -1,8 +1,11 @@
-
 import requests
+import os
+from dotenv import load_dotenv
 
-API_KEY = 'pIp1mK0nyI7T7mL7jRbvp3FdUR7zD7ypaXN4ktcl'
-url = "https://api.sportradar.com/nhl/trial/v7/en/seasons/2023/REG/leaders.json?api_key=pIp1mK0nyI7T7mL7jRbvp3FdUR7zD7ypaXN4ktcl"
+load_dotenv()
+API_KEY = os.getenv('API_KEY')
+
+url = f"https://api.sportradar.com/nhl/trial/v7/en/seasons/2023/REG/leaders.json?api_key={API_KEY}"
 
 headers = {"accept": "application/json"}
 
