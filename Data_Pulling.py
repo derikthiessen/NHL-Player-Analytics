85011d65287c15d6e585b4e32db23a98c33ad7cc
Derik Thiessen <derikt03@live.com>
2024-08-24 16:22:11 -0400

    Started structure for function to pull all game ids

--- Data_Pulling.py
+++ Data_Pulling.py
@@ -5,10 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
-url = f"https://api.sportradar.com/nhl/trial/v7/en/seasons/2023/REG/leaders.json?api_key={API_KEY}"
+url = f'https://api.sportradar.com/nhl/trial/v7/en/games/2024/01/06/schedule.json?api_key={API_KEY}'
 
 headers = {"accept": "application/json"}
 
 response = requests.get(url, headers=headers)
 
-print(response.text)
+data = response.json()
+
+print(data)
